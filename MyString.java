c0942b25368153ad4f7d2de36dcbb87af5ca38ac
amitbarha <amitbar131@gmail.com>
2025-11-25 10:24:13 +0200

    task 3

--- MyString.java
+++ MyString.java
@@ -9,7 +9,7 @@ public class MyString {
         System.out.println("Testing contains:");
         System.out.println(contains("unhappy", "happy")); // true
         System.out.println(contains("happy", "unhappy")); // false
-        System.out.println(contains("historical", "story")); // false
+        System.out.println(contains("history", "stf")); // false
         System.out.println(contains("psychology", "psycho")); // true
         System.out.println(contains("personality", "son")); // true
         System.out.println(contains("personality", "dad")); // false
@@ -18,13 +18,48 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String newStr = "";
+        for (int i = 0; i < str.length(); i++) {
+            if((str.charAt(i)>= 'A') && str.charAt(i)<= 'Z'){
+                char lower =(char) (str.charAt(i) + 32);
+                newStr += lower;
+            }
+            else{
+                newStr += str.charAt(i);
+            }
+            
+        }
+
+        return newStr;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+        if (str2.length()>str1.length()) {
+            return false;
+        }
+        if(str2.length()==0)
+        {
+            return true;
+        }
+        char firstCharStr2 = str2.charAt(0);
+        for (int i = 0; i <= str1.length()-str2.length(); i++) {
+            char current = str1.charAt(i);
+            if (current == firstCharStr2) {
+                boolean match = true;
+                for (int j = 1; j < str2.length(); j++) {
+                    current = str1.charAt(i + j);
+                    if (current != str2.charAt(j)) {
+                        match = false;
+                        break;
+                    }
+                }
+                if (match) {
+                    return true;
+                }
+
+            }
+        }
         return false;
     }
 }
